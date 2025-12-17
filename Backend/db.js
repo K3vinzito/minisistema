5aa016415350d688f750ed9baac380a92a231b24
Kevin Mendoza <kevin.1999.mc@gmail.com>
2025-12-17 10:24:11 -0500

    Config MySQL URL for production

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,22 @@
 // Backend/db.js
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+let pool;
 
-  port: process.env.DB_PORT || 3306,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+if (process.env.MYSQL_URL) {
+  // PRODUCCIÓN (Railway / Render)
+  pool = mysql.createPool(process.env.MYSQL_URL);
+} else {
+  // LOCAL
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
 
 module.exports = pool;
