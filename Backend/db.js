5a387a17ed2ace350f58fba8ec6e27ffd8f3d44c
Kevin Mendoza <kevin.1999.mc@gmail.com>
2025-12-17 10:47:19 -0500

    fix: mysql railway connection

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,22 @@
 // Backend/db.js
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool(process.env.MYSQL_URL);
+let pool;
+
+if (process.env.MYSQL_URL) {
+  // PRODUCCIÓN (Railway / Render)
+  pool = mysql.createPool(process.env.MYSQL_URL);
+} else {
+  // LOCAL
+  pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
 
 module.exports = pool;
-
