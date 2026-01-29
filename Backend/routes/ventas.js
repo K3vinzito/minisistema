129d57400c26d5de02a90a2b5d22f2a2a7881ac5
Kevin Mendoza <kevin.1999.mc@gmail.com>
2026-01-29 09:28:16 -0500

    backend ventas(autorizacion)

--- Backend/routes/ventas.js
+++ Backend/routes/ventas.js
@@ -1,6 +1,7 @@
 import express from "express";
 import pool from "../db.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 router.post("/orden", authMiddleware, async (req, res) => {
